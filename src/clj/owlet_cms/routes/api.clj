68cca5466ff95b57a9fe8d536cf0f01ea408ea6b
dviramontes <dviramontes@gmail.com>
2016-04-02 18:01:39 -0600

    webhook now checks if user exists otherwise insert

--- src/clj/owlet_cms/routes/api.clj
+++ src/clj/owlet_cms/routes/api.clj
@@ -14,26 +14,34 @@
         true
         false))))
 
-(defn handle-user-upsert-webhook [res]
+(defn handle-user-insert-webhook [res]
   (let [user (get-in res [:params :user])
         ;; context (get-in res [:params :context])
-        _ (clojure.pprint/pprint user)
-        transact! (try
-                    (if (is-not-social-login-but-verified? user)
-                      (jdbc/with-db-transaction
-                        [t-conn *db*]
-                        (jdbc/db-set-rollback-only! t-conn)
-                        (db/create-user!
-                          {:id       (:user_id user)
-                           :name     (:name user)
-                           :nickname (:nickname user)
-                           :email    (:email user)
-                           :picture  (:picture user)})))
-                    (catch Exception e (str "caught e:" (.getNextException e))))]
-    ;; returns 1 if inserted
-    (println transact!)
-    (when transact!
-      (ok user))))
+        found (try
+                (jdbc/with-db-transaction
+                  [t-conn *db*]
+                  (jdbc/db-set-rollback-only! t-conn)
+                  (db/get-user {:id (:user_id user)}))
+                (catch Exception e (str "caught e:" (.getNextException e))))]
+    (clojure.pprint/pprint user)
+    (if-not found
+      (let [transact! (try
+                        (if (is-not-social-login-but-verified? user)
+                          (jdbc/with-db-transaction
+                            [t-conn *db*]
+                            (jdbc/db-set-rollback-only! t-conn)
+                            (db/create-user!
+                              {:id       (:user_id user)
+                               :name     (:name user)
+                               :nickname (:nickname user)
+                               :email    (:email user)
+                               :picture  (:picture user)})))
+                        (catch Exception e (str "caught e:" (.getNextException e))))]
+        ;; returns 1 if inserted
+        (if transact!
+          (ok user)
+          (internal-server-error transact!)))
+      (ok "user exists"))))
 
 (defn handle-get-users [_]
   (let [users (jdbc/with-db-transaction
@@ -46,13 +54,14 @@
 (defn update-users-district-id! [res]
   (let [district_id (get-in res [:params :district-id])
         user_id (get-in res [:params :user-id])
-        transaction! (jdbc/with-db-transaction [t-conn *db*]
-                                     (jdbc/db-set-rollback-only! t-conn)
-                                     (db/update-user-district-id! {:district_id district_id
-                                                                   :id          user_id}))]
+        transaction! (jdbc/with-db-transaction
+                       [t-conn *db*]
+                       (jdbc/db-set-rollback-only! t-conn)
+                       (db/update-user-district-id! {:district_id district_id
+                                                     :id          user_id}))]
     (if (= 1 transaction!)
       (ok "updated user's district id")
-      (do 
+      (do
         (println transaction!)
         (internal-server-error transaction!)))))
 
@@ -60,6 +69,6 @@
            (context "/api" []
                     (GET "/users" [] handle-get-users)
                     (PUT "/users-district-id" {params :params} update-users-district-id!)
-                    (PUT "/webhook" {params :params} handle-user-upsert-webhook)))
+                    (PUT "/webhook" {params :params} handle-user-insert-webhook)))
 
 
